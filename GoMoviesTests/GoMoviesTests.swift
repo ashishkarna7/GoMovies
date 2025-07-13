e183f1ccdc9118cf5abcad905363dca4be05f14d
Ashish Karna <ashish.karna7@gmail.com>
2025-07-13 22:06:24 +0545

    Fix: Initail setup - added xcode project

---
+++ GoMoviesTests/GoMoviesTests.swift
@@ -0,0 +1,17 @@
+//
+//  GoMoviesTests.swift
+//  GoMoviesTests
+//
+//  Created by Ashish Karna on 13/07/2025.
+//
+
+import Testing
+@testable import GoMovies
+
+struct GoMoviesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
